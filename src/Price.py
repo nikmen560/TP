adfe0393691e744e30c1e6414b4677564ec46b34
Nikita <Kent720p@gmail.com>
2021-06-11 19:17:07 +0300

    add id to Price class

--- src/Price.py
+++ src/Price.py
@@ -1,6 +1,7 @@
 class Price:
-	def __init__(self, value, productId, customerId, date) -> None:
+	def __init__(self, value, productId, customerId, date, id) -> None:
 	    self.value = value
 	    self.productId = productId
 	    self.customerId = customerId
 	    self.date = date
+	    self.id = id
