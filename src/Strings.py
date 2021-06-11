a52097d7528bf87b613f84574fc21ba541df8400
Nikita <Kent720p@gmail.com>
2021-06-11 10:33:40 +0300

    created Strings file, that contains general UI commands and errors messages

---
+++ src/Strings.py
@@ -0,0 +1,13 @@
+startMenu = '''
+1. 
+2. 
+3. 
+4. 
+5. Выход
+'''
+
+#errors 
+
+errorNoInt = 'ошибка: введите число\n'
+errorWrongInput = 'ошибка: неверное значение\n'
+errorEmptyInput = 'ошибка: пустой ввод\n'
