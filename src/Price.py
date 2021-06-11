0f15b8e162549c0ffddc6275ec551e3d12b9b03d
Nikita <Kent720p@gmail.com>
2021-06-11 10:34:50 +0300

    created basic classes with constructors

---
+++ src/Price.py
@@ -0,0 +1,6 @@
+class Price:
+	def __init__(self, value, productId, customerId, date) -> None:
+	    self.value = value
+	    self.productId = productId
+	    self.customerId = customerId
+	    self.date = date
---
+++ src/Product.py
@@ -0,0 +1,5 @@
+class Product:
+	def __init__(self, name, priceId, productId) -> None:
+	    self.name = name 
+	    self.productId = productId
+	    self.priceId = priceId
