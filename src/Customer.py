615431a53a084e6bfee567ce114e795893cee85c
Nikita <Kent720p@gmail.com>
2021-06-11 10:15:07 +0300

    initial commit

---
+++ src/Customer.py
@@ -0,0 +1,7 @@
+class Customer:
+	def __init__(self, name, surname, address, id = None) -> None:
+	    self.name = name
+	    self.surname = surname
+	    self.address = address
+	    self.id = id
+	    
